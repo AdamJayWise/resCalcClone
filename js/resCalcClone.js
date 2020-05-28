c82564666e86869351e23ec00419fab759f01551
AdamWise <wise.adam.jay@gmail.com>
2020-05-28 13:10:57 -0400

    more updates, added spectrometers

--- js/resCalcClone.js
+++ js/resCalcClone.js
@@ -1,5 +1,31 @@
 console.log('resCalcClone (C)2020 Adam Wise');
 
+// Utility functions //
+function calculateTilt(obj){
+    sinTilt = 10**-6 * obj.centerWavelength * obj.grooveDensity / (-2 * Math.cos( (2 * Math.PI) * obj.deviationAngle / 360  ))
+    return 360 * Math.asin(sinTilt) / (2 * Math.PI)
+}
+
+function deg(angleInRadians){
+    return 360 * angleInRadians / ( 2 * Math.PI )
+}
+
+function rad(angleInDegrees){
+    return 2 * Math.PI * angleInDegrees / ( 360 )
+}
+
+
+function calcPixFactor(pixwidth){
+    if (pixwidth<14) return 1
+    if ( (pixwidth>=14) & (pixwidth<=50) ) return 0.4338*(pixwidth**0.3253);
+    if (pixwidth>50) return 1.55
+}
+
+// rounding function with specific number of decimal places d
+function r(n,d){
+    return Math.round(n*10**d)/10**d;
+}
+// end of utility functions //
 
 app = {
     'centerWavelength' : 500,
@@ -22,6 +48,14 @@ availableModels = {
         'Newton 970 EMCCD' : {'xPixels' : 1600, 'yPixels' : 200, 'pixelSize' : 16},
         'Newton 971 EMCCD' : {'xPixels' : 1600, 'yPixels' : 400, 'pixelSize' : 16},
     },
+
+    'istar' : {},
+
+    'idus' : {},
+
+    'ikonm' : {},
+
+    'scmos' :{},
 };
 
 var cameraButtons = d3.selectAll('.cameraChoiceButton');
@@ -37,10 +71,17 @@ cameraButtons.on('click', function(d,i){
     Object.keys(availableModels[family]).forEach(function(key){
         console.log(key)
         var camObj = availableModels[family][key];
-        app['activeCamObj'] = camObj;
         var newRow = d3.select('#modelInfoTable').append('tr').classed('tableBodyRow', true);
         var firstCell = newRow.append('td')
-        firstCell.append('input').property('type', 'radio');
+        var radio = firstCell.append('input')
+            .property('type', 'radio')
+            .property('name', 'camera')
+            .property('value', key);
+        
+        radio.on('change', function(){
+            app['activeCameraObj'] = camObj;
+            updateResults();
+        })
         firstCell.append('span').text(key);
     
         newRow.append('td').text(camObj['xPixels']+' x '+camObj['yPixels']);
@@ -49,15 +90,70 @@ cameraButtons.on('click', function(d,i){
     
 })
 
+// invoke a click for the first button
+d3.select('.cameraChoiceButton').node().click();
+
 
 // spectrometer selection callback
-d3.selectAll('.spectrometerChoiceButton')
+var spectrometerButtons = d3.selectAll('.spectrometerChoiceButton');
+spectrometerButtons.on('click', function(d,i){
+    
+    console.log(this.getAttribute('value'));
+    var spectrometer = this.getAttribute('value');
+    app['activeSpectrometer'] = spectrometer;
+    spectrometerObj = spectrometers[spectrometer];
+    app['activeSpectrometerObj'] = spectrometerObj;
+
+    // update the spectrometer parameters table
+    d3.select('#spectrometerNameDisplay').text( (spectrometer + ' SPECTROGRAPH').toUpperCase() );
+
+    // update the rows
+    d3.select('#spectrometerInfoTable').selectAll('.dataRow').remove();
+
+    var row1 = d3.select('#spectrometerInfoTable').append('tr').classed('dataRow', true);
+    row1.append('td').text('Focal Length')
+    row1.append('td').text(app['activeSpectrometerObj']['fl'] + ' mm');
+
+    var row2 = d3.select('#spectrometerInfoTable').append('tr').classed('dataRow', true);
+    row2.append('td').text('Aperture')
+    row2.append('td').text(app['activeSpectrometerObj']['f#']);
+
+    var row3 = d3.select('#spectrometerInfoTable').append('tr').classed('dataRow', true);
+    row3.append('td').text('Grating Size')
+    row3.append('td').text(app['activeSpectrometerObj']['gratingSizeX'] + ' x ' +  app['activeSpectrometerObj']['gratingSizeX'] + ' mm'   );
+
+    
+
+})
+
+//invoke a click for the first button
+d3.select('.spectrometerChoiceButton').node().click();
+
+
+// center wavelength input callback with validation
+d3.select('#centerWavelengthInput').on('change', function(){
+    console.log(this.value)
+    var newWavelength = Number(this.value) || 500;
+    
+    if (newWavelength < 0){
+        newWavelength = 0;
+    }
+    
+    if (newWavelength > 2000){
+        newWavelength = 2000;
+    }
+
+    this.value = newWavelength;
+    app['centerWavelength'] = newWavelength;
+    updateResults();
+
+})
 
 //
 function updateResults(){
     //update center wavelength on results table
     d3.select("#centerWavelengthNmTd").text(app['centerWavelength']);
-    d3.select("#centerWavelengtheVTd").text(1240/app['centerWavelength']);
+    d3.select("#centerWavelengtheVTd").text( r(1240/app['centerWavelength'], 4) );
     
 }
 
--- js/spectrometers.js
+++ js/spectrometers.js
@@ -1,10 +1,24 @@
 var spectrometers = { 
-    
+
+                'Shamrock 163' : {//'psf' : 60, // original psf from Tristan's script, in um
+                                'psf' : 59.16, // point spread function after removal of 10um slit 
+                                'dev' : -13.9, // deviation in degrees
+                                'fpt' : 4, // focal plane tilt, in degrees
+                                'fl' : 163, // focal length, in mm
+                                'gratingSizeX' : '45',
+                                'gratingSizeY' : '45',
+                                'f#' : 3.6,
+                                'displayName' : 'Shamrock 163', // what to call it in the GUI,
+                },
+
                 'Kymera 193' : {//'psf' : 60, // original psf from Tristan's script, in um
                                 'psf' : 59.16 * 0.98989, // point spread function after removal of 10um slit 
                                  'dev' : -14, // deviation in degrees
                                 'fpt' : 4.56, // focal plane tilt, in degrees
                                 'fl' : 193, // focal length, in mm
+                                'gratingSizeX' : '45',
+                                'gratingSizeY' : '45',
+                                'f#' : 3.6,
                                 'displayName' : 'Kymera 193', // what to call it in the GUI
                                 },
 
@@ -13,6 +27,9 @@ var spectrometers = {
                                 'dev' : -10.875,
                                'fpt' : 5,
                                'fl' : 328,
+                               'gratingSizeX' : '80',
+                               'gratingSizeY' : '80',
+                               'f#' : 4.1,
                                'displayName' : 'Kymera 328'
                                }, 
 
@@ -21,6 +38,9 @@ var spectrometers = {
                                'dev' : -10.875,
                               'fpt' : 5,
                               'fl' : 328,
+                              'gratingSizeX' : '80',
+                              'gratingSizeY' : '80',
+                              'f#' : '?',
                               'displayName' : 'Kymera 328 with TruRes'
                               }, 
 
@@ -29,6 +49,9 @@ var spectrometers = {
                             'dev' : -11.5,
                             'fpt' : 3.752,
                             'fl' : 500,
+                            'gratingSizeX' : '80',
+                            'gratingSizeY' : '80',
+                            'f#' : 6.5,
                             'displayName' : 'Shamrock 500'
                               }, 
                 
@@ -37,6 +60,9 @@ var spectrometers = {
                             'dev' : -7.39,
                             'fpt' : 1.083,
                             'fl' : 750,
+                            'gratingSizeX' : '77',
+                            'gratingSizeY' : '77',
+                            'f#' : 9.7,
                             'displayName' : 'Shamrock 750'
                              }, 
                 };
